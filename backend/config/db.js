a1f427ab0de7328e546e8b081e44410b3ed56cdd
Tarun5311 <anumolu.tarunkumar247@gmail.com>
2023-05-14 03:09:10 +0530

    Connected MongoDB

---
+++ backend/config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+const colors = require('colors')
+
+
+const connectDB = async() => {
+    try{
+        await mongoose.connect(process.env.mongoUrl)
+        console.log(`MongoDb connected ${mongoose.connection.host}`)
+    } catch (error){
+        console.log(`MongoDb Server Issue ${error}`)
+    }
+}
+
+module.exports = connectDB;
