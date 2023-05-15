b3612fccd5de6870a1b464e4a77ecf0dde258177
Tarun5311 <anumolu.tarunkumar247@gmail.com>
2023-05-15 17:45:04 +0530

    Updated dependencies and Updated homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,118 @@
-import {Routes, Route, Switch, Router } from "react-router-dom";
-import './App.css';
-import LoginPage from './components/LoginPage';
-import RegisterPage from './components/RegisterPage';
-import HomePage from './components/HomePage';
-import SearchPage from './components/SearchPage';
-import ProfilePage from "./components/ProfilePage";
-import RowCard from "./components/RowCard";
+import "./styles/app.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { Toaster } from "react-hot-toast";
+import { Protected, Public, Admin } from "./middleware/route";
+import React, { lazy, Suspense } from "react";
+import Loading from "./components/Loading";
 
+const Home = lazy(() => import("./pages/Home"));
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const Appointments = lazy(() => import("./pages/Appointments"));
+const Doctors = lazy(() => import("./pages/Doctors"));
+const Profile = lazy(() => import("./pages/Profile"));
+const Notifications = lazy(() => import("./pages/Notifications"));
+const ApplyDoctor = lazy(() => import("./pages/ApplyDoctor"));
+const Error = lazy(() => import("./pages/Error"));
 
 function App() {
-  const user = {
-    name: 'John Doe',
-    photoUrl: 'https://example.com/profile.jpg',
-    qualification: 'Software Engineer',
-    bio: 'I am a software engineer with 5 years of experience in web development.',
-  };
-  
   return (
-    
-		<Routes>
-      <Route path="/login" element={<LoginPage/>}/>
-      <Route path="/register" element={<RegisterPage/>} />
-      <Route path="/homepage" element={<HomePage/>} />
-      <Route path="/search" element={<SearchPage/>} />
-      <Route path="/profile" element={<ProfilePage/>} />
-    </Routes>
+    <Router>
+      <Toaster />
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route
+            path="/login"
+            element={<Login />}
+          />
+          <Route
+            path="/register"
+            element={
+              <Public>
+                <Register />
+              </Public>
+            }
+          />
+          <Route
+            path="/"
+            element={<Home />}
+          />
+          <Route
+            path="/doctors"
+            element={<Doctors />}
+          />
+          <Route
+            path="/appointments"
+            element={
+              <Protected>
+                <Appointments />
+              </Protected>
+            }
+          />
+          <Route
+            path="/notifications"
+            element={
+              <Protected>
+                <Notifications />
+              </Protected>
+            }
+          />
+          <Route
+            path="/applyfordoctor"
+            element={
+              <Protected>
+                <ApplyDoctor />
+              </Protected>
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <Protected>
+                <Profile />
+              </Protected>
+            }
+          />
+          <Route
+            path="/dashboard/users"
+            element={
+              <Admin>
+                <Dashboard type={"users"} />
+              </Admin>
+            }
+          />
+          <Route
+            path="/dashboard/doctors"
+            element={
+              <Admin>
+                <Dashboard type={"doctors"} />
+              </Admin>
+            }
+          />
+          <Route
+            path="/dashboard/appointments"
+            element={
+              <Protected>
+                <Dashboard type={"appointments"} />
+              </Protected>
+            }
+          />
+          <Route
+            path="/dashboard/applications"
+            element={
+              <Protected>
+                <Dashboard type={"applications"} />
+              </Protected>
+            }
+          />
+          <Route
+            path="*"
+            element={<Error />}
+          />
+        </Routes>
+      </Suspense>
+    </Router>
   );
 }
 
